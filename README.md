a4489bc0a5f9c6a7c0eaf3333db3185664229ceb
kwiecien <k.d.kwiecinski@gmail.com>
2019-10-11 10:20:48 +0200

    add follow up questions to the welcome intent

---
+++ README.md
@@ -0,0 +1,13 @@
+# JAM Assistant
+
+## Setup
+```shell script
+cd src
+firebase use <PROJECT-ID>
+firebase deploy
+```
+
+## Links
+- https://dialogflow.cloud.google.com
+- https://console.actions.google.com
+- https://console.firebase.google.com
--- src/functions/index.js
+++ src/functions/index.js
@@ -3,7 +3,9 @@
 // Import the Dialogflow module and response creation dependencies from the Actions on Google client library.
 const {
     dialogflow,
-    Permission,
+    Suggestions,
+    Carousel,
+    Image,
 } = require('actions-on-google');
 
 // Import the firebase-functions package for deployment.
@@ -12,18 +14,50 @@ const functions = require('firebase-functions');
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
 
+const getYesNoSuggestions = () => new Suggestions('Yes', 'No');
+
+const getFaqCarousel = () => new Carousel({
+    items: {
+        'agenda': {
+            title: 'Agenda',
+            synonyms: ['agenda', 'plan', 'events'],
+            image: new Image({
+                url: 'https://images.unsplash.com/photo-1506784242126-2a0b0b89c56a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1348&q=80',
+                alt: 'Agenda Picture',
+            }),
+        },
+        'hotel': {
+            title: 'Hotel',
+            synonyms: ['hotel', 'room', 'accommodation'],
+            image: new Image({
+                url: 'https://images.unsplash.com/photo-1504652517000-ae1068478c59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+                alt: 'Hotel Picture',
+            }),
+        },
+        'projectile': {
+            title: 'Projectile',
+            synonyms: ['projectile', 'book', 'hours'],
+            image: new Image({
+                url: 'https://images.unsplash.com/photo-1504197832061-98356e3dcdcf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+                alt: 'Projectile Picture',
+            }),
+        },
+    },
+});
+
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent('Default Welcome Intent', (conv) => {
-    const name = conv.user.storage.userName;
-    if (!name) {
-        // Asks the user's permission to know their name, for personalization.
-        conv.ask(new Permission({
-            context: 'Hi there, to get to know you better',
-            permissions: 'NAME',
-        }));
-    } else {
-        conv.ask(`Hi again, ${name}. What's your favorite color?`);
-    }
+    conv.ask(`Hi! JAM 2019 is coming. Do you have any questions about it?`);
+    conv.ask(getYesNoSuggestions());
+});
+
+app.intent('Default Welcome Intent - no', (conv) => {
+    conv.close('Okay, but as soon as you have any questions, just let me know. Have a nice day!');
+});
+
+app.intent('Default Welcome Intent - yes', (conv) => {
+    conv.ask('What would you like to know?');
+    conv.ask(getFaqCarousel());
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
